21a850c094163669d97c5c21ad0b9da232a29f19
mcodemax <mwalterjohnson7@gmail.com>
2021-04-21 08:58:51 -0700

    added populateEpisodes func

--- tvmaze.js
+++ tvmaze.js
@@ -18,18 +18,40 @@
       }
  */
 async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
+  // TODO: Make an ajax request to the se+archShows api.  Remove
   // hard coded data.
 
-  return [
-    {
+  // get() this first for a show id http://api.tvmaze.com/search/shows?q=girls
+  //then use the abv id here to http://api.tvmaze.com/shows/${showid}/episodes
+  const res = await axios.get('http://api.tvmaze.com/search/shows', { params: {q: query} });
+ 
+  //use the map method to map all shows coming from the search into an array
+
+  let showsArr = res.data;
+  showsArr = showsArr.map(e => {
+    return{ 
+    id: e.show.id,
+    name: e.show.name,
+    summary: e.show.summary,
+    image: e.show.image.medium ? e.show.image.medium : ' https://tinyurl.com/tv-missing'
+    }
+  });
+
+
+  return showsArr;
+}
+
+/*    { what to return like for the abv f()
       id: 1767,
       name: "The Bletchley Circle",
       summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
       image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
-}
+    }*/
+
+  //make a loop, go thru array and pick the highest score, or just pick the first relevancy since it's the score
+  //const showID = res.data[0].show.id;
+ // const showEPs = await axios.get(`http://api.tvmaze.com/shows/${showID}/episodes`);
+  //console.log(showEPs)
 
 
 
@@ -85,6 +107,24 @@ async function getEpisodes(id) {
   // TODO: get episodes from tvmaze
   //       you can get this by making GET request to
   //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+  const epList = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+  console.log(epList.data);
 
+  return epList.data;
   // TODO: return array-of-episode-info, as described in docstring above
 }
+
+
+function populateEpisodes(epArr){
+  document.querySelector('#episodes-area').removeAttribute('style');
+
+  //make a loop that ataches each ep to dom
+  for(let x of epArr){
+    $('#episodes-list').append(`<li>${x.name} (Season ${x.season} Episode ${x.number}</li>`);
+    console.log(x)
+  }
+}
+
+// populateEpisodes([{name: 'name', season: 2, number: 5},
+// {name: 'name', season: 2, number: 5},
+// {name: 'name', season: 2, number: 5}]); //test code
